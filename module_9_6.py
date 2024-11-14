3542462cd962146b01d62e693af16db962de7f4a
microsizer201a <dimanalter@rambler.ru>
2024-11-14 14:59:35 +0300

    Add files via upload

--- module_9_6.py
+++ module_9_6.py
@@ -1,21 +1,18 @@
 def all_variants(text):
-
     s = 0
-    e = 0
-    ctn = 0
+    e = 1
+    ctn = 1
     while text[s: e] != text:
-        if text[e] == text[len(text)-1]:
+        yield text[s: e]
+        if e == len(text):
             s = 0
             ctn += 1
             e = ctn
-            yield text[s: e]
         else:
             s += 1
             e += 1
-            yield text[s: e]
+    yield text
 
-
-
-a = all_variants("abc")
+a = all_variants("abcde")
 for i in a:
     print(i)
